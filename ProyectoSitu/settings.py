b911521e4ad5378254246b373f5a17bcb1a82db4
dilan.chamba <166522911+Dilan1615@users.noreply.github.com>
2026-05-24 18:04:52 -0500

    fix allowed hosts

--- ProyectoSitu/settings.py
+++ ProyectoSitu/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-e$5&*94q(tb$(ko4s&hd3_g7s%*&-^wit6*d)6zt3gov#0$uo@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "djangositu-bacsh3bfguaqgfbr.centralus-01.azurewebsites.net",
+]
 
 # Application definition
 
